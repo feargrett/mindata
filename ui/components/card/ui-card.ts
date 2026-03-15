1c0977836f570dc90246670671b6b49d3021aeca
feargrett <agustin.8956g@gmail.com>
2026-03-15 16:17:25 -0300

    ui card added force vertical align

--- ui/components/card/ui-card.ts
+++ ui/components/card/ui-card.ts
@@ -20,15 +20,17 @@ export class UiCardComponent {
   label = input<string>('Label');
   title = input<string>('Title');
   description = input<string>('Description');
-  buttonLabel = input<string>('Click me');
+  buttonLabel = input<string>();
   buttonDisabled = input<boolean>(false);
   showShadow = input<boolean>(true);
   withPadding = input<boolean>(true);
+  align = input<'auto' | 'vertical'>('auto');
 
   cardClasses = computed(() => {
     const classes = [];
     if (this.showShadow()) classes.push('card--shadow');
     if (this.withPadding()) classes.push('card--padding');
+    if (this.align() === 'vertical') classes.push('card--vertical');
     return classes.join(' ');
   });
 }
